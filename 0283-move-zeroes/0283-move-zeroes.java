4bf3b0a4d66fd37afb0f9551b1b9d67f553a655d
pathuri sujith reddy <vtu26220@veltech.edu.in>
2026-02-26 09:44:11 +0530

    Time: 2 ms (90.21%), Space: 47.6 MB (93.34%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0; // position for next non-zero
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // swap nums[i] with nums[j]
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
