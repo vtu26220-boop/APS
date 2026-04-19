1c89a5ff13334fba090f812572ea2e505ab38c5b
pathuri sujith reddy <vtu26220@veltech.edu.in>
2026-04-19 18:45:24 +0530

    Time: 0 ms (100%), Space: 42.4 MB (59.3%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        
+        for(int i = 0; i < 32; i++) {
+            result <<= 1;        // shift result left
+            result |= (n & 1);   // add last bit of n
+            n >>= 1;             // shift n right
+        }
+        
+        return result;
+    }
+}
